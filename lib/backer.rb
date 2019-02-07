3c2fb8456ea0ad0afdc6e3ddb69c7f706de7bba2
Alexandra Palermo <alexmpalermo@gmail.com>
2019-02-07 22:54:58 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -10,7 +10,7 @@ class Backer
   
   def back_project(project)
     @backed_projects << project
-    project.add_backer(self) unless backed_projects.include?(self)
+    project.add_backer(self) unless backed_projects.include?(project)
 end
   
 end 
